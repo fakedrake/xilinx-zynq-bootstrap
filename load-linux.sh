684d0f867711e2226826d73edc7aac40544fdcc8
Chris Perivolaropoulos <darksaga2006@gmail.com>
2013-02-18 16:29:53 +0200

    Added the linux loader :D

---
+++ load-linux.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ -d "resources/" ]; then
+    cd resources/
+else
+    echo "No `pwd`/resources/ dir found."
+    exit 0
+fi
+
+echo "connect arm hw
+source ps7_init.tcl
+ps7_init
+init_user
+source stub.tcl
+target 64
+dow -data uImage            0x30000000
+dow -data uramdisk.img.gz   0x20000000
+dow -data zynq-zc702.dtd    0x2A000000
+dow u-boot.elf
+con
+" | xmd && echo -e "\n" > /dev/ttyUSB0 && echo "bootm 0x3000000 0x2000000 0x2A00000" > /dev/ttyUSB0
+
+echo "Running sudo minicom -D /dev/ttyUSB0 -b 115200"
