a8182ffd721e4ced5f957e08d675b31fd521af9d
Chris Perivolaropoulos <darksaga2006@gmail.com>
2013-10-25 18:18:44 +0300

    Dont mount from a random address, esoecially sice you have already mounted.

--- data/fdinit.sh
+++ data/fdinit.sh
@@ -2,7 +2,6 @@
 
 mknod /dev/fb0 c 29 0
 
-mount 192.168.1.109:/home/fakedrake/Projects/ThinkSilicon/xilinx-zynq-bootstrap/fs/ /homes/fs/ -o rw,nolock
 mount -o bind /proc /homes/fs/proc
 mount -o bind /dev /homes/fs/dev
 
