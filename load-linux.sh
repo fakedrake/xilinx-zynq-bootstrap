c45dde655c429bd6aa26a19394724418511198e2
Chris Perivolaropoulos <darksaga2006@gmail.com>
2014-07-03 17:34:27 +0300

    Split in functions cleaned unused functionality.

--- load-linux.sh
+++ load-linux.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
+# export LD_LIBRARY_PATH=/tools/Xilinx/ISE-SE_Viv-SE/14.4/ISE_DS/EDK/lib/lin64:/tools/Xilinx/ISE-SE_Viv-SE/14.4/ISE_DS/ISE/lib/lin64
+# export XILINX=/tools/Xilinx/ISE-SE_Viv-SE/14.4/ISE_DS/ISE
+# export XILINX_EDK=/tools/Xilinx/ISE-SE_Viv-SE/14.4/ISE_DS/EDK
+# export XILINXD_LICENSE_FILE=/tools/licenses/Xilinx_Zynq-7000_EPP_ZC702_IMAGEON_gray.lic
+
+# XLNX=/tools/Xilinx/ISE-SE_Viv-SE/14.4
+# # /tools/Xilinx/ISE/13.2
+# export XILINX=$XLNX/ISE_DS/ISE
+# export XILINX_EDK=$XLNX/ISE_DS/EDK
+# XILINX_BIN_PATH=$XLNX/ISE_DS/EDK/bin/lin
+# XILINX_BIN_PATH64=$XLNX/ISE_DS/EDK/bin/lin64
+
+REMOTE_XMD="ssh grey"
+XMD=/opt/Xilinx/SDK/2013.3/bin/lin64/xmd
+
 function fail {
     echo "[ERROR] Error while $1"
     exit 0
 }
 
-RUN_MINICOM='false'
-BOOT_LINUX='true'
-RESET_DEVICE='false'
 read -d '' HELP_MESSAGE <<EOF
 
 This program loads your software to the board. I assume the current
@@ -17,21 +29,7 @@ successfully.
 OPTIONS:
 --reset		Reset the device
 
---no-minicom	Do not run minicom connected to the serial.
-
---with-minicom	Run minicom at the end. [default]
-
---no-boot-linux	Do not boot linux, only u-boot.
-
---boot-linux	Make u-boot boot linux [default]
-
---serial DEVICE	Override serial device autodetection.
-
---which-serial	Show which serial we would use. The script is not run.
-
---xmd FILE	Override xmd executable autodetection.
-
---which-xmd	Show which xmd we would use. The script is not run.
+--minicom	Only run minicom.
 
 --xmd-shell 	Run an xmd shell. Try to have readline with rlwrap.  It is
 recommended to run xmd from here anyway as it will try
@@ -39,137 +37,174 @@ to correct your xmd executable aswell
 
 EOF
 
-XILINX_BIN_PATH=/orange-opt/Xilinx/14.4/ISE_DS/EDK/bin/lin
-XILINX_BIN_PATH64=${XILINX_BIN_PATH}64
-
 LOG_FILE="load-linux.log"
 
-echo "Beginning Script" > $LOG_FILE
 
-# Get the xmd executable
-if [ -d $XILINX_BIN_PATH64 ] && [ $(uname -p) = 'x86_64' ]; then
-    XMD=$XILINX_BIN_PATH64/xmd
-elif [ -d $XILINX_BIN_PATH ]; then
-    XMD=$XILINX_BIN_PATH/xmd
-else
-    # Try the PATH
-    XMD=$(which xmd)
-fi
+function setup_xmd {
+    # Get the xmd executable
 
-if [ ! $SERIAL ]; then
-    SERIAL=$( ls -d /dev/* | grep ttyUSB | head -1 )
-    echo "Using serial: $SERIAL"
-fi
+    open_xmd="$($REMOTE_XMD pgrep xmd)"
+    if [ -n "$open_xmd" ]; then
+	fail "Looks like another xmd is running with pid=$open_xmd. Found with: $REMOTE_XMD pgrep xmd"
+    fi
 
+    if [ -n "$XMD" ]; then
+	echo "Override any xmd detection with '$XMD'";
+    elif [ -d $XILINX_BIN_PATH64 ] && [ $(uname -p) = 'x86_64' ]; then
+	XMD=$XILINX_BIN_PATH64/xmd
+    elif [ -d $XILINX_BIN_PATH ]; then
+	XMD=$XILINX_BIN_PATH/xmd
+    else
+	echo "Failed to find xmd at $XILINX_BIN_PATH64 and $XILINX_BIN_PATH, trying \$PATH"
+	# Try the PATH
+	XMD=$(which xmd)
+    fi
+
+    if [ -n "$REMOTE_XMD" ]; then
+	XMD="$REMOTE_XMD $XMD"
+    fi
+
+
+    if [ -z "$XMD" ]; then
+	echo "No xmd found."
+	exit 0
+    fi
+    echo "XMD: $XMD"
+}
+
+function setup_serial
+{
+    if [ ! $SERIAL ]; then
+	SERIAL=$( ls -d /dev/* | grep ttyUSB | head -1 )
+	echo "Using serial: $SERIAL"
+    fi
+
+
+    if [ ! $SERIAL ] || [ ! -c $SERIAL ]; then
+	echo "No serial port found or the provided is invalid."
+	exit 0
+    fi
+
+    [ ! -w $SERIAL ] && echo "Serial $SERIAL is not writeable. Try 'sudo chmod a+w $SERIAL'" && exit 0
+    [ ! -r $SERIAL ] && [ "$RUN_MINICOM" = "true" ] && \
+    echo "Serial $SERIAL is not readable. Cannot open minicom on it. Try 'sudo chmod a+rw $SERIAL'" && exit 0
+}
+
+function xmd_shell
+{
+    if [ $(command -v rlwrap) ]; then
+	echo "Using rlwrap for history and completion, you are welcome."
+	rlwrap $XMD
+    else
+	echo "rlwrap not found, running plain xmd"
+	$XMD
+    fi
+}
+
+
+function reset_device
+{
+    echo "echo \"Device reset commanded by $(whoami)!\"" > $SERIAL
+    echo -e "connect arm hw\ntarget 64\nrst" | $XMD
+}
+
+function print_xmd_commands
+{
+    resources=$(pwd)/resources
+    if ! [[ -d $resources ]]; then
+	fail "No `pwd`/resources/ dir found."
+    fi
+
+    uimage=$resources/uImage
+    ramdisk=$resources/uramdisk.img.gz
+    dtb=$resources/zynq-zc702.dtb
+    ubootelf=$resources/u-boot.elf
+    ps7_init_tcl=$resources/ps7_init.tcl
+    stub_tcl=$resources/stub.tcl
+
+    echo "connect arm hw
+source $ps7_init_tcl
+ps7_init
+init_user
+source $stub_tcl
+debugconfig -memory_access_check disable
+target 64
+
+dow $ubootelf
+
+dow -data $uimage	0x30000000
+dow -data $ramdisk	0x20000000
+dow -data $dtb		0x2A000000
+con
+"
+}
+
+function boot_linux {
+    # In order to have interactive output you may want to make a named pipe for this
+    print_xmd_commands | tee -a $LOG_FILE | $XMD || fail "sending images to device"
+
+    # sleep 1
+
+    # echo -e "\n" > $SERIAL
+
+    # sleep 4
+    # echo "bootm 0x30000000 0x20000000 0x2A000000" > $SERIAL
+}
+
+function minicom {
+    MINICOM_CMD="/usr/bin/minicom -D $SERIAL -b 115200"
+    echo "Running $MINICOM_CMD"
+    $MINICOM_CMD
+}
+
+
+function main
+{
+    echo "Beginning Script" > $LOG_FILE
+    setup_serial
+    setup_xmd
+    boot_linux
+    echo "Ending Script" > $LOG_FILE
+
+    [ -z "$no_minicom" ] && minicom
+}
 
 while [[ $# -gt 0 ]]; do
     case $1 in
 	'--reset')
-	    RESET_DEVICE='true';;
-	'--no-minicom')
-	    RUN_MINICOM='false';;
-	'--with-minicom')
-	    RUN_MINICOM='true';;
-	'--boot-linux')
-	    BOOT_LINUX='true';;
-	'--no-boot-linux')
-	    BOOT_LINUX='false';;
-	'--serial')
-	    shift
-	    [ $1 ] && [ -c $1 ] && SERIAL="$1" || echo "Invalid serial device: $1";;
-	'--xmd')
-	    shift
-	    [ $1 ] && [ -x $1 ] && XMD='$1' || echo "Invalid xmd: $1";;
+	    setup_serial;
+	    setup_xmd;
+	    reset_device;
+	    exit 0;;
+	'--minicom')
+	    setup_serial;
+	    minicom;
+	    exit 0;;
 	'--which-serial')
+	    setup_serial;
 	    echo "$SERIAL"
-	    exit 1;;
+	    exit 0;;
 	'--which-xmd')
+	    setup_xmd;
 	    echo "$XMD"
-	    exit 1;;
+	    exit 0;;
 	'--xmd-shell')
-	    XMD_SHELL='true';;
+	    setup_xmd;
+	    xmd_shell;
+	    exit 0;;
+	'--xmd-commands')
+	    print_xmd_commands;
+	    exit 0;;
+	'--no-minicom')
+	    no_minicom="y";;
 	'--help')
 	    echo "$HELP_MESSAGE"
-	    exit 1;;
+	    exit 0;;
 	*)
 	    echo "Unrecognized option \"$1\"";
-	    exit 0;;
+	    exit 1;;
     esac
     shift
 done
 
-
-if [ ! $XMD ] || [ ! -x $XMD ]; then
-    echo "I couldnt locate xmd at $XMD or is not executable"
-    exit 0
-fi
-echo "XMD executable: $XMD"
-
-if  [ "$XMD_SHELL" = 'true' ]; then
-    if [ $(command -v rlwrap) ]; then
-	echo "Using rlwrap for history and completion, you are welcome."
-	rlwrap $XMD
-	exit 1
-    else
-	echo "rlwrap not found, running plain xmd"
-	$XMD
-	exit 1
-    fi
-fi
-
-if [ ! $SERIAL ] || [ ! -c $SERIAL ]; then
-    echo "No serial port found or the provided is invalid."
-    exit 0
-fi
-
-[ ! -w $SERIAL ] && echo "Serial $SERIAL is not writeable. Try 'sudo chmod a+w $SERIAL'" && exit 0
-[ ! -r $SERIAL ] && [ "$RUN_MINICOM" = "true" ] && echo "Serial $SERIAL is not readable. Cannot open minicom on it. Try 'sudo chmod a+rw $SERIAL'" && exit 0
-
-if [ "$RESET_DEVICE" = 'true' ]; then
-    echo "echo \"Device reset commanded by $(whoami)!\"" > $SERIAL
-    echo -e "connect arm hw\nrst" | $XMD
-    exit
-fi
-
-if [[ -d resources/ ]]; then
-    cd resources/
-    echo "In directory: `pwd`"
-else
-    echo "No `pwd`/resources/ dir found."
-    exit 0
-fi
-
-
-if [ "$RUN_MINICOM" = "true" ]; then
-    MINICOM_CMD="minicom -D $SERIAL -b 115200"
-    echo "Running $MINICOM_CMD"
-    $MINICOM_CMD
-fi
-
-UIMAGE=uImage
-RAMDISK=uramdisk.img.gz
-DTB=zynq-zc702.dtb
-UBOOTELF=u-boot.elf
-PS7_INIT_TCL=ps7_init.tcl
-STUB_TCL=stub.tcl
-
-# In order to have interactive output you may want to make a named pipe for this
-echo "connect arm hw
-source $PS7_INIT_TCL
-ps7_init
-init_user
-source $STUB_TCL
-target 64
-dow -data $UIMAGE	0x30000000
-dow -data $RAMDISK	0x20000000
-dow -data $DTB		0x2A000000
-dow $UBOOTELF
-con
-" | tee -a $LOG_FILE | $XMD || fail "sending images to device"
-sleep 1
-echo -e "\n" > $SERIAL
-
-if [ "$BOOT_LINUX" = 'true' ]; then
-    sleep 2
-    echo "bootm 0x30000000 0x20000000 0x2A000000" > $SERIAL
-fi
+main
